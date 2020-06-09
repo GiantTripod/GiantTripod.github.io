ec5b40acd80bdcf35c9f0fe0abf81de790988a0c
GiantTripod <46637005+GiantTripod@users.noreply.github.com>
2020-06-09 11:53:29 -0600

    class changes

--- main.js
+++ main.js
@@ -1,49 +1,50 @@
-function showPage1() {
-    document.querySelector('#page-1').style.display = '';
-    document.querySelector('#page-2').style.display = 'none';
-    document.querySelector('#page-3').style.display = 'none';
-    document.querySelector('#page-4').style.display = 'none';
-    document.querySelector('#page-5').style.display = 'none';
-}
-
-function showPage2() {
-    document.querySelector('#page-1').style.display = 'none';
-    document.querySelector('#page-2').style.display = '';
-    document.querySelector('#page-3').style.display = 'none';
-    document.querySelector('#page-4').style.display = 'none';
-    document.querySelector('#page-5').style.display = 'none';
-}
-
-function showPage3() {
-    document.querySelector('#page-1').style.display = 'none';
-    document.querySelector('#page-2').style.display = 'none';
-    document.querySelector('#page-3').style.display = '';
-    document.querySelector('#page-4').style.display = 'none';
-    document.querySelector('#page-5').style.display = 'none';
-}
-
-function showPage4() {
-    document.querySelector('#page-1').style.display = 'none';
-    document.querySelector('#page-2').style.display = 'none';
-    document.querySelector('#page-3').style.display = 'none';
-    document.querySelector('#page-4').style.display = '';
-    document.querySelector('#page-5').style.display = 'none';
-}
-
-function showPage5() {
-    document.querySelector('#page-1').style.display = 'none';
-    document.querySelector('#page-2').style.display = 'none';
-    document.querySelector('#page-3').style.display = 'none';
-    document.querySelector('#page-4').style.display = 'none';
-    document.querySelector('#page-5').style.display = '';
-}
-
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
+    function showPage1() {
+        document.querySelector('#page-1').style.display = '';
+        document.querySelector('#page-2').style.display = 'none';
+        document.querySelector('#page-3').style.display = 'none';
+        document.querySelector('#page-4').style.display = 'none';
+        document.querySelector('#page-5').style.display = 'none';
     }
-  } 
+
+    function showPage2() {
+        document.querySelector('#page-1').style.display = 'none';
+        document.querySelector('#page-2').style.display = '';
+        document.querySelector('#page-3').style.display = 'none';
+        document.querySelector('#page-4').style.display = 'none';
+        document.querySelector('#page-5').style.display = 'none';
+    }
+
+    function showPage3() {
+        document.querySelector('#page-1').style.display = 'none';
+        document.querySelector('#page-2').style.display = 'none';
+        document.querySelector('#page-3').style.display = '';
+        document.querySelector('#page-4').style.display = 'none';
+        document.querySelector('#page-5').style.display = 'none';
+    }
+
+    function showPage4() {
+        document.querySelector('#page-1').style.display = 'none';
+        document.querySelector('#page-2').style.display = 'none';
+        document.querySelector('#page-3').style.display = 'none';
+        document.querySelector('#page-4').style.display = '';
+        document.querySelector('#page-5').style.display = 'none';
+    }
+
+    function showPage5() {
+        document.querySelector('#page-1').style.display = 'none';
+        document.querySelector('#page-2').style.display = 'none';
+        document.querySelector('#page-3').style.display = 'none';
+        document.querySelector('#page-4').style.display = 'none';
+        document.querySelector('#page-5').style.display = '';
+    }
+
+
+    function myFunction() {
+        var nav = document.getElementById("myNavigation");
+        if (nav.className === "navigation") {
+        nav.className += " responsive";
+        } else {
+        nav.className = "navigation";
+        }
+    } 
+
